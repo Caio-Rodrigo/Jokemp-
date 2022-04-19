b4766f62bc5227d44bda4582687f34261ee71f33
Caio Rodrigo Silva <78828999+Caio-Rodrigo@users.noreply.github.com>
2022-04-19 20:42:03 -0300

    inicio
    inicio

---
+++ main.js
@@ -0,0 +1,11 @@
+console.clear();
+console.log('<----------Jokempô---------->');
+
+const    prompt  = require('prompt-sync');
+let      rodadas = 0
+let      escolha =['Pedra', 'Papel', 'Tesoura']
+let      pc
+let      eu
+
+
+

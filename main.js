caaac5104643acd56f3b58b61b5ae6274f8cb21a
Caio Rodrigo Silva <78828999+Caio-Rodrigo@users.noreply.github.com>
2022-04-19 23:02:30 -0300

    recover

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 console.clear();
 console.log('<----------Jokempô---------->');
 
-const    prompt  = require('prompt-sync');
-let      rodadas = 0
-let      escolha =['Pedra', 'Papel', 'Tesoura']
-let      pc
-let      eu
+let prompt = require("prompt-sync")();
+let rodadas 
+let escolha =['Pedra', 'Papel', 'Tesoura']
+let pc
+let eu
 
 
 
+rodadas = +prompt('Quantas rodadas você deseja jogar?');
+
+while (isNaN(rodadas) /*&& rodadas != Number.isInteger(rodadas)*/) {
+    rodadas = +prompt('Insira um valor numerico: ');
+}
+console.log(rodadas);
